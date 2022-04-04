41209a61cc4cad7bb736d81420de6e20b8efb58c
sarinasindurakar <sarinasindurakar82@gmail.com>
2022-04-04 22:42:11 +0545

    form validation

--- register.py
+++ register.py
@@ -10,8 +10,8 @@ import datetime
 import time
 from PIL import Image, ImageTk
 import facedetection
-   
-    
+from tkinter import messagebox  
+import re
 def registerme():
     window=tk.Tk()
     
@@ -105,35 +105,65 @@ def registerme():
     #tkinter variable for storing entries
     
     
+    #--------------------validation----------------------
+    def validate(ID):
+        if ID.isdigit():
+            return True
+        if len(str(ID))== 0:
+            return True
+        else:
+            messagebox.showwarning("Invalid","Invalid Entry")
+            return False
     
+    my_valid = window.register(validate)
     
     lbl1= tk.Label(frame4, text="Enter ID",width=8  ,height=1  ,fg="black",font=('times', 17, ' bold ') )
     lbl1.place(x=140, y=55)
-    txt_id = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '))
+    txt_id = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '),validate='key',validatecommand=(my_valid,'%S'))
     txt_id.place(x=280, y=55)
     
-    
+    def validate_name(name):#for ID and batch validation
+        if name.isalpha():
+            return True
+        if name == "":
+            return True	
+        else:
+            messagebox.showwarning("Invalid","Only Alphabet is allowed") 
+            return False
+    valid_name = window.register(validate_name)
     lbl2= tk.Label(frame4, text="Name",width=8 ,height=1  ,fg="black",font=('times', 17, ' bold ') )
     lbl2.place(x=140, y=90)
     
-    txt_name = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '))
+    txt_name = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '),validate='key',validatecommand=(valid_name,'%P'))
     txt_name.place(x=280, y=90)
     
-    
-    lbl3= tk.Label(frame4, text="Faculty",width=8  ,height=1  ,fg="black",font=('times', 17, ' bold ') )
-    lbl3.place(x=140, y=125)
-    txt_faculty = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '))
-    txt_faculty.place(x=280, y=125)
-    
-    lbl4= tk.Label(frame4, text="Batch",width=8  ,height=1  ,fg="black",font=('times', 17, ' bold ') )
-    lbl4.place(x=140, y=160)
-    txt_batch = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '))
-    txt_batch.place(x=280, y=160)
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    def email_validation(email):
+        if(re.search(regex,email) and email.isalpha):
+            return True 
+        if email == "":
+            return True	
+        else:
+            messagebox.showwarning("Alert","Invalid E-mail enter by user")
+            return False
+    my_validmail=window.register(email_validation)  
     
     lbl5= tk.Label(frame4, text="Email",width=8  ,height=1  ,fg="black",font=('times', 17, ' bold ') )
-    lbl5.place(x=140, y=195)
-    txt_email = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '))
-    txt_email.place(x=280, y=195)
+    lbl5.place(x=140, y=125)
+    txt_email = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '),validate='focusout',validatecommand=(my_validmail,'%P'))
+    txt_email.place(x=280, y=125)
+   
+    lbl3= tk.Label(frame4, text="Faculty",width=8  ,height=1  ,fg="black",font=('times', 17, ' bold ') )
+    lbl3.place(x=140, y=160)
+    txt_faculty = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '),validate='key',validatecommand=(valid_name,'%S'))
+    txt_faculty.place(x=280, y=160)
+    
+    lbl4= tk.Label(frame4, text="Batch",width=8  ,height=1  ,fg="black",font=('times', 17, ' bold ') )
+    lbl4.place(x=140, y=195)
+    txt_batch = tk.Entry(frame4,width=20,fg="black",font=('times', 15, ' bold '),validate='key',validatecommand=(my_valid,'%S'))
+    txt_batch.place(x=280, y=195)
+    
+    
     
     button_save =tk.Button(frame4, text="Take Image and Save",width="20",height="2", fg="red",command=takeimage_full)
     button_save.place(x=200,y=250)
